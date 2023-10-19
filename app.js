db5fe1739b5ce28aff7afd391c0b0840a4ea7ce5
zor009 <anzohra@outlook.com>
2023-10-19 15:46:31 -0400

    added math floor to our new function getrandomcolor

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const color= document.querySelector(".color");
 
 btn.addEventListener("click",function() {
     //get random number btw index zero and 3 so i[]
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber()
+    console.log(randomNumber);
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
+});
 
-})
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length); 
+} 
