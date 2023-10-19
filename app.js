ba60980cb06b7a5c237081f35b1d41ec0ea31cfb
zor009 <anzohra@outlook.com>
2023-10-19 15:52:09 -0400

    added to hex html and js

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ btn.addEventListener("click",function() {
 });
 
 function getRandomNumber(){
-    return Math.floor(Math.random() * colors.length); 
+    return Math.floor(Math.random() * colors.length); //math.floor(Math.random.. is used to round randomNumber to thenearest interger.
 } 
